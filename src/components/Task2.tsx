2353fc976c4a5ec9a2fee8d0a0fb1b38088a8022
Pavel Lisitsyn <43790794+LisPavel@users.noreply.github.com>
2024-02-03 21:02:19 +0300

    feat(useLocalStorage): add value and setItem function

--- src/components/Task2.tsx
+++ src/components/Task2.tsx
@@ -2,18 +2,20 @@ import React from "react";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 
 export function Task2() {
-  // const [token, { setItem, removeItem }] = useLocalStorage('token');
+  const [token, { setItem /* removeItem */ }] =
+    useLocalStorage<string>("token");
 
   return (
     <div>
-      <p>{/* Твой токен: { token } */}</p>
+      <p>Твой токен: {token}</p>
       <div>
-        {/* <button onClick={() => setItem('new-token')}>
-          Задать токен
-        </button>
+        {
+          <button onClick={() => setItem("new-token")}>Задать токен</button>
+          /* 
         <button onClick={() => removeItem()}>
           Удалить токен
-        </button> */}
+        </button> */
+        }
       </div>
     </div>
   );
--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,5 +1,39 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
-export function useLocalStorage<T>(value: T) {
-  const [state, setState] = useState<T>(value);
+const getValueStorage = <T>(
+  key: string,
+  initialState?: (() => T) | T,
+): T | undefined => {
+  const localStorageValue = localStorage.getItem(key);
+  if (localStorageValue) {
+    return JSON.parse(localStorageValue) as T;
+  }
+  if (initialState instanceof Function) {
+    return initialState();
+  }
+  return initialState;
+};
+
+export function useLocalStorage<T>(
+  key: string,
+  initialValue?: T,
+): [
+  T | undefined,
+  {
+    setItem: (newValue: T) => void;
+  },
+] {
+  const [value, setValue] = useState<T | undefined>(() =>
+    getValueStorage<T>(key, initialValue),
+  );
+
+  const setItem = useCallback(
+    (newValue: T) => {
+      localStorage.setItem(key, JSON.stringify(newValue));
+      setValue(newValue);
+    },
+    [key],
+  );
+
+  return [value, { setItem }];
 }
