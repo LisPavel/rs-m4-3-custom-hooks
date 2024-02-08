6a885562099f39a45d47c41887c93d544148399c
Pavel Lisitsyn <43790794+LisPavel@users.noreply.github.com>
2024-02-08 23:51:58 +0300

    feat: make realization useToggle with array parameter

--- src/components/additional/Task2.tsx
+++ src/components/additional/Task2.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { useToggle } from "../../hooks/useToggle";
 
+const colors = ["red", "blue", "cyan", "teal"] as const;
+
 export function AdditionalTask2() {
-  // const [value, toggle] = useToggle(["blue", "orange", "cyan", "teal"]);
-  const [value, toggle] = useToggle(false);
+  const [value, toggle] = useToggle(colors);
 
   return (
     <>
       <div>{value.toString()}</div>
       <button onClick={() => toggle()}>Toggle</button>
-      <button onClick={() => toggle(true)}>true</button>
-      <button onClick={() => toggle(false)}>false</button>
-      {/* <button onClick={() => toggle()}>{value}</button> */}
-      {/* <button onClick={() => toggle("blue")}>blue</button> */}
+      {colors.map((c) => (
+        <button key={c} onClick={() => toggle(c)}>
+          {c}
+        </button>
+      ))}
     </>
   );
 }
--- src/hooks/useToggle.tsx
+++ src/hooks/useToggle.tsx
@@ -1,13 +1,65 @@
-import { useState } from "react";
+import { useReducer } from "react";
 
-export function useToggle(
-  initialValue?: boolean,
-): [boolean, (value?: boolean) => void] {
-  const [value, setValue] = useState(initialValue ?? false);
+enum ToggleActionKind {
+  TOGGLE = "TOGGLE",
+}
 
-  function toggleValue(value?: boolean) {
-    setValue((prevState) => (typeof value === "boolean" ? value : !prevState));
+interface ToggleAction<T> {
+  type: ToggleActionKind;
+  payload?: T;
+}
+
+interface ToggleState<T> {
+  items: T[];
+  active: number;
+}
+
+const defaultItems = [false, true];
+
+function toggleReducer<T>(
+  state: ToggleState<T>,
+  action: ToggleAction<(typeof state.items)[number]>,
+): ToggleState<T> {
+  const { type, payload } = action;
+
+  switch (type) {
+    case ToggleActionKind.TOGGLE: {
+      if (payload == null) {
+        return {
+          ...state,
+          active:
+            state.active + 1 === state.items.length ? 0 : state.active + 1,
+        };
+      }
+      return {
+        ...state,
+        active: state.items.indexOf(payload),
+      };
+    }
+    default:
+      return state;
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type GetItemType<T extends ArrayLike<any>> = T[number];
+
+export function useToggle<T = boolean>(
+  toggleArray: ArrayLike<T> = defaultItems as T[],
+): [
+  GetItemType<typeof toggleArray>,
+  (value?: GetItemType<typeof toggleArray>) => void,
+] {
+  type PossibleValue = (typeof toggleArray)[number];
+
+  const [state, dispatch] = useReducer(toggleReducer<T>, {
+    active: 0,
+    items: toggleArray,
+  } as ToggleState<T>);
+
+  function toggleValue(value?: PossibleValue): void {
+    dispatch({ type: ToggleActionKind.TOGGLE, payload: value });
   }
 
-  return [value, toggleValue];
+  return [state.items[state.active], toggleValue];
 }
